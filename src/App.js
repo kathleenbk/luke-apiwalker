ed3413e81ea1e617f21b48a19dfb0cea978fdd83
kathleenbk <katie.kava3@gmail.com>
2022-05-12 12:57:34 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import SearchForm from './components/form';
+import People from './components/people';
+import Planets from './components/planets';
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Link, Routes, Route, Navigate } from "react-router-dom";
+
 
 function App() {
+
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchForm />
+      <Routes>
+        <Route path="/people/:id" element={<People />} />
+        <Route path="/planets/:id" element={<Planets />} />
+      </Routes>
+
     </div>
+    </BrowserRouter>
   );
 }
 
